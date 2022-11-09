df3c703ca6b939bbb25c69443eae93668d21d945
Rhys Mills <rhystmills@gmail.com>
2022-11-09 10:39:29 +0000

    Get react2angular running without errors

--- index.tsx
+++ index.tsx
@@ -2,6 +2,8 @@ import { IAugmentedJQuery, IComponentOptions } from 'angular'
 import fromPairs = require('lodash.frompairs')
 import NgComponent from 'ngcomponent'
 import * as React from 'react'
+import type { ReactElement } from 'react'
+import { useEffect, useState } from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
 
 /**
@@ -54,3 +56,79 @@ export function react2angular<Props>(
     }]
   }
 }
+
+/**
+ * Creates a store to allow state within React to be programmatically updated
+ * outside of a React context.
+ */
+export const createStore = <Value extends unknown>(initialValue: Value): { update: (val: Value) => boolean, Store: Store<Value> } => {
+  type Subscription = (val: Value) => void
+
+  const subscribers: Subscription[] = []
+
+  const store = {
+    currentValue: initialValue,
+
+    /**
+     * Get the current value of the store.
+     */
+    getValue: () => store.currentValue,
+
+    /**
+     * Update the value in the store, informing subscribers.
+     *
+     * @return {boolean} true if there are any subscribers, false if not.
+     */
+    update: (val: Value): boolean => {
+      store.currentValue = val
+      subscribers.forEach((sub) => sub(store.getValue()))
+      return subscribers.length ? true : false
+    },
+
+    /**
+     * Subscribe to store updates.
+     */
+    subscribe: (sub: Subscription) => subscribers.push(sub),
+
+    /**
+     * Unsubscribe to store updates.
+     */
+    unsubscribe: (sub: Subscription) => {
+      const subToRemove = subscribers.findIndex(
+        (currentSub) => currentSub === sub
+      )
+      subscribers.splice(subToRemove, 1)
+    }
+  }
+
+  /**
+   * The Store component takes a single function as a child component, and calls it
+   * with the store value. It will call this function every time the store value is
+   * updated, rerendering its children.
+   *
+   * Example usage:
+   *
+   * ```tsx
+   * <Store>
+   *   {(value) =>
+   *      <div>{value}</div>
+   *   }
+   * </Store>
+   * ```
+   */
+  const Store = ({ children }: { children: (val: Value) => ReactElement }) => {
+    const [value, setValue] = useState(store.getValue())
+    useEffect(() => {
+      store.subscribe(setValue)
+      return () => store.unsubscribe(setValue)
+    }, [])
+
+    return children(value)
+  }
+
+  return { update: store.update, Store }
+}
+
+export type Store<Value> = (props: {
+  children: (val: Value) => ReactElement;
+}) => ReactElement
